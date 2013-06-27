24c318e90b8f6eb2f3b350025981946f2d5a7889
Ryan Sepassi <rsepassi@gmail.com>
2013-06-27 10:19:13 -0700

    updated

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,31 +5,36 @@ class Feed < ActiveRecord::Base
 
   def self.find_or_create_by_url(url)
     feed = Feed.find_by_url(url)
-    unless feed
-      begin
-        feedData = SimpleRSS.parse(open(url))
-        feed = Feed.create!(title: feedData.title, url: url)
-        feedData.entries.each do |entryData|
-          Entry.create_from_json!(entryData, feed)
-        end
-      rescue SimpleRSSError
-        return nil
+    return feed if feed
+
+    begin
+      feed_data = SimpleRSS.parse(open(url))
+      feed = Feed.create!(title: feed_data.title, url: url)
+      feed_data.entries.each do |entry_data|
+        Entry.create_from_json!(entry_data, feed)
       end
+    rescue SimpleRSSError
+      return nil
     end
+
     feed
   end
 
   def reload
-    # should reload entries
+    # reloads entries
     begin
-      feedData = SimpleRSS.parse(url)
-      self.title = feedData.title
+      feed_data = SimpleRSS.parse(url)
+      self.title = feed_data.title
       save!
-      feedData.entries.each do |entryData|
-        unless Entry.find_by_guid(entryData.guid)
-          Entry.create_from_json!(entryData, feed)
+
+      existing_entry_guids = Entry.pluck(:guid).sort
+      feed_data.entries.each do |entry_data|
+        unless existing_entry_guids.include?(entry_data.guid)
+          Entry.create_from_json!(entry_data, feed)
         end
       end
+
+      self
     rescue SimpleRSSError
       return false
     end
